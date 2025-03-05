d1bf4cc4396641bd78470d2086fff5cdaefd5d7b
Bhunesh ojha108 <149822963+Bhuneshojha@users.noreply.github.com>
2025-03-05 21:05:23 +0500

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Growth Mindset App", layout="centered")
+st.set_page_config(page_title="Growth Mindset App", )
 st.title("🌱 Growth Mindset App")
 
 # Hero Section
